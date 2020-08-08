9861aa740d109d3b64ae5caf1febd46b26e0459e
devJRL <dev2sponge@gmail.com>
2020-08-08 16:54:21 +0900

    Render with value of query in context

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -15,8 +15,18 @@ const CountryHome = ({ shows }) => {
 }
 
 // Fetching Data on Server side [Next] - https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
-CountryHome.getInitialProps = async () => {
-  const res = await fetch('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+CountryHome.getInitialProps = async context => {
+  // Dynamic Context Fetching
+  const country = context.query.country || 'us'
+
+  // [ISO-3166-1 :: Country code] https://en.wikipedia.org/wiki/ISO_3166-1
+  console.log("Dynamic Context catching - country:", country) // logged on Server, not on client
+  // http://localhost:3000/ca >> Dynamic Context catching - country: ca
+  // http://localhost:3000/br >> Dynamic Context catching - country: br
+
+  const res = await fetch(
+    `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
+  )
   const json = await res.json();
   return { shows: json }
 }
