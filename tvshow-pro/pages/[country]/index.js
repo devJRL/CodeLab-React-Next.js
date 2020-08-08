fff82455e33006ee7866c68b069c8f70af264d30
devJRL <dev2sponge@gmail.com>
2020-08-08 16:08:00 +0900

    Fetching Data on Server side (props & fetch)

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,16 +1,6 @@
-import axios from 'axios' // > [AXIOS] `yarn add axios`
-// import { useEffect } from 'react'
-
 const CountryHome = (props) => {
   // Fetching Data on Server side [Next]
   console.log("CountryHome -> props.shows:", props.shows)
-
-  // Fetching Data on Client side [Axios]
-  // useEffect(() => {
-  //   axios
-  //     .get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-  //     .then(response => console.log(response.data))
-  // }, [])
 
   return (
     <h1>This is Country Test page.</h1>
@@ -20,9 +10,9 @@ const CountryHome = (props) => {
 // Fetching Data on Server side [Next]
 // https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
 CountryHome.getInitialProps = async () => {
-  const response = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-  // console.log(response.data)
-  return { shows: await response.data }
+  const res = await fetch('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+  const json = await res.json();
+  return { shows: json }
 }
 
 // http://localhost:3000/[us]
