b921fbcf5f3613888c98abf83a662b65e0c03e08
devJRL <dev2sponge@gmail.com>
2020-08-08 15:29:04 +0900

    Fetching Data on Client Side (axios)

--- tvshow-pro/pages/[country]/index.js
+++ tvshow-pro/pages/[country]/index.js
@@ -1,4 +1,18 @@
-const CountryTest = () => <h1>This is Country Test page.</h1>
+import { useEffect } from 'react'
+import axios from 'axios' // > [AXIOS] `yarn add axios`
+
+const CountryHome = () => {
+  useEffect(() => {
+    // Fetching Data on Client side [Axios]
+    axios
+      .get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+      .then(response => console.log(response.data))
+  }, [])
+
+  return (
+    <h1>This is Country Test page.</h1>
+  )
+}
 
 // http://localhost:3000/[us]
-export default CountryTest;
+export default CountryHome;
