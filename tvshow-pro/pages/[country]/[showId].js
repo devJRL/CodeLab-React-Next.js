80cc2ffa214ae0aadce3bd3138fcb9ced75da4cc
devJRL <dev2sponge@gmail.com>
2020-08-08 13:59:01 +0900

    Dynamic route example

---
+++ tvshow-pro/pages/[country]/[showId].js
@@ -0,0 +1,4 @@
+const ShowDetail = () => <h1>This is Detail page.</h1>
+
+// http://localhost:3000/[us]/[123]
+export default ShowDetail;
---
+++ tvshow-pro/pages/[country]/index.js
@@ -0,0 +1,4 @@
+const CountryTest = () => <h1>This is Country Test page.</h1>
+
+// http://localhost:3000/[us]
+export default CountryTest;
