0d6e5aaf794931fb9c98a33193376c4565b79ab1
devJRL <dev2sponge@gmail.com>
2020-08-11 22:28:25 +0900

    ShowDetail on static

--- tvshow-pro/pages/[country]/[showId].js
+++ tvshow-pro/pages/[country]/[showId].js
@@ -1,4 +1,30 @@
-const ShowDetail = () => <h1>This is Detail page.</h1>
+import axios from 'axios'
+
+const ShowDetail = ({ show }) => {
+  const { name, image } = show
+
+  return (
+    <div className="show-details">
+      <hi>{name}</hi>
+      <div
+        className="show-details__poster"
+        style={{ backgroundImage: `url(${image.original})` }}
+      ></div>
+
+      <style jsx>{`
+        .show-details__poster {
+          height: 200px;
+          background-size: cover;    
+        }
+      `}</style>
+    </div>
+  )
+}
 
 // http://localhost:3000/[us]/[123]
 export default ShowDetail;
+
+ShowDetail.getInitialProps = async () => {
+  const res = await axios.get(`http://api.tvmaze.com/shows/1?embed=cast`)
+  return { show: res.data }
+}
