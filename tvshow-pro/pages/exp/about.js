ee12d162e01749670deb4f1ff2716ee05f552bc1
devJRL <dev2sponge@gmail.com>
2020-08-11 21:51:40 +0900

    next/Link component with <a> tag

--- tvshow-pro/pages/exp/about.js
+++ tvshow-pro/pages/exp/about.js
@@ -1,4 +1,17 @@
-const About = () => <h1>This is our About page.</h1>
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is our About page.</h2>
+      <p>
+        <Link href="/">
+          <a>Go home('/') 'Link' component.</a>
+        </Link>
+      </p>
+    </div>
+  )
+}
 
 // localhost:3000/exp/about
 export default About
--- tvshow-pro/pages/index.js
+++ tvshow-pro/pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
-      This is my Home Page.
+      <h2>This is my Home Page.</h2>
+      <p>
+        <Link href="/exp/about">
+          <a>About('/exp/about') with 'Link' component.</a>
+        </Link>
+      </p>
     </div>
   )
 }
