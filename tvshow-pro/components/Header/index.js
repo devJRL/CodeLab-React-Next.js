771b850d52346114245abbe9e9b9d2e4a2f1ad62
devJRL <dev2sponge@gmail.com>
2020-08-13 20:23:19 +0900

    Match selected option along with 'query'

--- tvshow-pro/components/Header/index.js
+++ tvshow-pro/components/Header/index.js
@@ -1,9 +1,26 @@
+import { useRouter } from 'next/router';
+
 const countries = [
   { label: 'us', name: 'United States' },
   { label: 'bz', name: 'Brazil' },
 ]
 
 const Header = () => {
+  // Catch [country] in qeury from Next-Router
+  const router = useRouter()
+  // router: ServerRouter {
+  //   route: '/[country]',
+  //   pathname: '/[country]',
+  //   query: { country: 'us' },
+  //   asPath: '/us',
+  //   basePath: '',
+  //   events: undefined,
+  //   isFallback: false
+  // }
+
+  // Match basic selected option along with 'query'
+  const countryPath = router.query.country
+
   // CallBack Function for <select> tag
   const selectorChange = e => {
     // console.log('selected country: ', e.target.value)
@@ -18,7 +35,9 @@ const Header = () => {
 
   return (
     <div className="header">
-      <select onChange={selectorChange}>
+      <select
+        value={countryPath}
+        onChange={selectorChange}>
         {renderCountries()}
       </select>
 
