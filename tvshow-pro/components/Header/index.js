35fef2f06fe53a7bab8c395262352bf78e5d4edc
devJRL <dev2sponge@gmail.com>
2020-08-13 20:08:03 +0900

    Add select tag (.map() of Array)

--- tvshow-pro/components/Header/index.js
+++ tvshow-pro/components/Header/index.js
@@ -1,14 +1,25 @@
+const countries = [
+  { label: 'us', name: 'United States' },
+  { label: 'bz', name: 'Brazil' },
+]
+
 const Header = () => {
   // CallBack Function for <select> tag
   const selectorChange = e => {
     // console.log('selected country: ', e.target.value)
   }
 
+  // Generate with map() of Array
+  const renderCountries = () => {
+    return countries.map(country => {
+      return <option key={country.label} value={country.label}>{country.name}</option>
+    })
+  }
+
   return (
     <div className="header">
       <select onChange={selectorChange}>
-        <option value="us">United States</option>
-        <option value="bz">Brazil</option>
+        {renderCountries()}
       </select>
 
       <style jsx>{`
